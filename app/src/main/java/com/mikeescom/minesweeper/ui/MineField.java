a20dc767c00b682f247b871b93bdc273d87b0ad0
mikeescom <mike_matematicas@ciencias.unam.mx>
2019-02-09 17:56:21 -0800

    Adding first listeners to buttons

--- app/src/main/java/com/mikeescom/minesweeper/ui/MineField.java
+++ app/src/main/java/com/mikeescom/minesweeper/ui/MineField.java
@@ -1,43 +1,28 @@
 package com.mikeescom.minesweeper.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import com.mikeescom.minesweeper.R;
-import com.mikeescom.minesweeper.utilities.Constants;
-
-import java.util.zip.Inflater;
 
 public class MineField extends AppCompatActivity {
 
-    private int mHorizontalSizeOfField;
+    private static final int mHorizontalSizeOfField = 12;
     private GridLayout mMineFiled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mine_field);
-        initData();
         initView();
         initMineField();
-
-    }
-
-    private void initData() {
-        Intent intent = getIntent();
-        mHorizontalSizeOfField = Integer.parseInt(intent.getStringExtra(Constants.INTENT_SIZE_OF_FIELD));
     }
 
     private void initView() {
@@ -53,9 +38,24 @@ public class MineField extends AppCompatActivity {
         int finalWidth = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, squareSize, displayMetrics);
         for (int i = 0; i < (mHorizontalSizeOfField * verticalNumberOfSquares) ; i++) {
             View view = getLayoutInflater().inflate(R.layout.square_layout, null);
+            final ImageView imageView = view.findViewById(R.id.image_button);
             ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(finalWidth, finalWidth);
             view.setLayoutParams(params);
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.covered));
             mMineFiled.addView(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.uncovered));
+                }
+            });
+            view.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.flaged));
+                    return true;
+                }
+            });
         }
     }
 }
